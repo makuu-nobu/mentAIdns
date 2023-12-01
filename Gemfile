e48554b6e75f9e9bba31fdb6c812a069f5a581de
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-12-01 18:44:00 +0900

    deviseビュー作成とfaker

--- Gemfile
+++ Gemfile
@@ -73,3 +73,4 @@ end
 
 gem 'pry-rails'
 gem 'devise'
+gem 'faker'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_validation :generate_email_and_password, on: :create
+  before_create :create_answer
 
   validates :release_option, presence: true
   
   has_many :answers
+
+
+  private
+
+  def generate_email_and_password
+    self.email = Faker::Internet.unique.email
+    self.password = Faker::Internet.password
+  end
+
+  def create_answer
+    build_answer(username: self.username)
+  end  
+
 end
