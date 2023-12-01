8b7edebcf56b2dd894975c0ff562034d9bd1fa94
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-12-01 16:51:04 +0900

    微調整

--- db/migrate/20231201074420_add_column_name_to_users.rb
+++ db/migrate/20231201074420_add_column_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnNameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :username, :string
-    add_column :users, :release_option, :integer
+    add_column :users, :username, :string, null: false
+    add_column :users, :release_option, :integer, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_01_074420) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.integer "release_option"
+    t.string "username", null: false
+    t.integer "release_option", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
