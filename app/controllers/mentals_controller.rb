e01471afd76ca787ff2a5eed45dd70f84e9e4c71
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-12-03 12:14:11 +0900

    フォームオブジェクト雛形

--- app/controllers/mentals_controller.rb
+++ app/controllers/mentals_controller.rb
@@ -2,6 +2,8 @@ class MentalsController < ApplicationController
     def index
     end
     def new
+        @form = Form.new
+        @form.question_text = "ここに問題文を表示しよう"
         @choices = ["Option 1", "Option 2", "Option 3", "Option 4"]
     end
     def create
